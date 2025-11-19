c0a7a8f778e8bcda069badeba89b70e440f611d8
Douglas Machado <douglasvilla23@gmail.com>
2025-11-18 23:46:38 -0300

    Primeiro commit

---
+++ src/main/java/com/bancomachado/bancomachado/BancomachadoApplication.java
@@ -0,0 +1,13 @@
+package com.bancomachado.bancomachado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancomachadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancomachadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancomachado/bancomachado/BancomachadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancomachado.bancomachado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancomachadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
