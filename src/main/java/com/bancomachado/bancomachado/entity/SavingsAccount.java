d44a5b4fd844fd8238e92f5b98e93c9ef8cd1418
Douglas Machado <douglasvilla23@gmail.com>
2025-11-25 23:37:51 -0300

    feat: cria a classe SavingsAccount para contas poupança

---
+++ src/main/java/com/bancomachado/bancomachado/entity/SavingsAccount.java
@@ -0,0 +1,29 @@
+package com.bancomachado.bancomachado.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class SavingsAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int agency;
+
+    private int account;
+
+    private Long balance;
+
+
+
+}
